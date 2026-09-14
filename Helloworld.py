78fadecc75ac80104d3290067122b04c18652309
lee427 <iwj070222@gmail.com>
2026-09-14 17:38:47 +0900

    Update Hello_world.py

--- Helloworld.py
+++ Helloworld.py
@@ -1,6 +1,9 @@
 def print_hello():
     print("Hello World!!")
 
-    
+def print_KIT():
+    print("Kumoh National Institute of Technology")
+
 if __name__ == '_main_':
+    print_KIT()
     print_hello()
